be84e60783b232f0d459ccc3d3adc5682fb810c3
edolev89 <edolev89@gmail.com>
2018-11-24 19:53:26 -0500

    ops

--- completer_torch.py
+++ completer_torch.py
@@ -21,7 +21,7 @@ parser.add_argument('--b2', type=float, default=0.999, help='adam: decay of firs
 parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
 parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
 parser.add_argument('--img_size', type=int, default=32, help='size of each image dimension')
-parser.add_argument('--channels', type=int, default=1, help='number of image channels')
+parser.add_argument('--channels', type=int, default=3, help='number of image channels')
 parser.add_argument('--sample_interval', type=int, default=400, help='interval between image sampling')
 parser.add_argument('--dataset', type=str, default='cifar10', help='interval between image sampling')
 opt = parser.parse_args()
