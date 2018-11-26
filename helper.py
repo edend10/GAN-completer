4cb2114768cb57a770b81d8e14e87ca3b08c7d86
edolev89 <edolev89@gmail.com>
2018-11-26 01:03:04 -0500

    helppper

--- helper.py
+++ helper.py
@@ -7,7 +7,7 @@ from torchnet.logger import VisdomPlotLogger
 
 def load_dataset(dataset_name, image_size, batch_size):
     path = 'data/' + dataset_name
-    normal_mean = (0.5, 0.5, 0.5)
+    normal_mean = (0, 0, 0)
     normal_std = (0.5, 0.5, 0.5)
     os.makedirs(path, exist_ok=True)
     if dataset_name == 'cifar10':
