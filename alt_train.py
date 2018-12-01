49363a0ab5c2ee3cb859cb54950c87b12aceab41
edolev89 <edolev89@gmail.com>
2018-11-30 19:07:25 -0500

    alternative

---
+++ alt_train.py
@@ -0,0 +1,165 @@
+import argparse
+import os
+import numpy as np
+from torchvision.utils import save_image, make_grid
+from torch.autograd import Variable
+import torch
+import helper
+from alt_models import Generator, Discriminator
+from visdom import Visdom
+
+os.makedirs('images', exist_ok=True)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
+parser.add_argument('--batch_size', type=int, default=64, help='size of the batches')
+parser.add_argument('--lr', type=float, default=0.0002, help='adam: learning rate')
+parser.add_argument('--b1', type=float, default=0.5, help='adam: decay of first order momentum of gradient')
+parser.add_argument('--b2', type=float, default=0.999, help='adam: decay of first order momentum of gradient')
+parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
+parser.add_argument('--latent_dim', type=int, default=100, help='dimensionality of the latent space')
+parser.add_argument('--img_size', type=int, default=32, help='size of each image dimension')
+parser.add_argument('--channels', type=int, default=3, help='number of image channels')
+parser.add_argument('--sample_interval', type=int, default=400, help='interval between image sampling')
+parser.add_argument('--dataset', type=str, default='cifar10', help='dataset name')
+parser.add_argument('--logging', type=bool, default=False, help='log or not')
+parser.add_argument('--log_port', type=int, default=8080, help='visdom log panel port')
+opt = parser.parse_args()
+print(opt)
+
+cuda = True if torch.cuda.is_available() else False
+
+if cuda:
+    print("Using Cuda!")
+else:
+    print("No Cuda :(")
+
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm2d') != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+
+
+# Logging
+if opt.logging:
+    d_real_loss_logger = helper.get_logger(opt.log_port, 'd_loss_real')
+    d_fake_loss_logger = helper.get_logger(opt.log_port, 'd_loss_fake')
+    d_total_loss_logger = helper.get_logger(opt.log_port, 'd_loss_total')
+    g_loss_logger = helper.get_logger(opt.log_port, 'g_loss')
+    viz_image_logger = Visdom(port=opt.log_port, env="images")
+
+# Loss function
+adversarial_loss = torch.nn.BCELoss()
+
+# Initialize generator and discriminator
+generator = Generator(opt.img_size, 128, opt.latent_dim, opt.channels)
+discriminator = Discriminator(opt.img_size, 128, opt.channels)
+
+
+if cuda:
+    generator.cuda()
+    discriminator.cuda()
+    adversarial_loss.cuda()
+
+# Initialize weights
+generator.apply(weights_init_normal)
+discriminator.apply(weights_init_normal)
+
+dataloader = helper.load_dataset(opt.dataset, opt.img_size, opt.batch_size)
+
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+# ----------
+#  Training
+# ----------
+
+for epoch in range(opt.n_epochs):
+    avg_g_loss = 0
+    avg_d_fake_loss = 0
+    avg_d_real_loss = 0
+    avg_d_total_loss = 0
+    epoch_batches = 1
+    for i, (imgs, _) in enumerate(dataloader):
+
+        # Adversarial ground truths
+        # valid = Variable(Tensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False)
+        # fake = Variable(Tensor(imgs.shape[0], 1).fill_(0.0), requires_grad=False)
+
+        # Try label smoothing
+        valid = Variable(Tensor(np.random.uniform(0.8, 1.2, (imgs.shape[0], 1))), requires_grad=False)
+        fake = Variable(Tensor(np.random.uniform(0.0, 0.15, (imgs.shape[0], 1))), requires_grad=False)
+
+        # Configure input
+        real_imgs = Variable(imgs.type(Tensor))
+
+        # -----------------
+        #  Train Generator
+        # -----------------
+
+        optimizer_G.zero_grad()
+
+        # Sample noise as generator input
+        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+
+        # Generate a batch of images
+        gen_imgs = generator(z)
+
+        # Loss measures generator's ability to fool the discriminator
+        g_loss = adversarial_loss(discriminator(gen_imgs), valid)
+
+        g_loss.backward()
+        optimizer_G.step()
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        optimizer_D.zero_grad()
+
+        # Measure discriminator's ability to classify real from generated samples
+        real_loss = adversarial_loss(discriminator(real_imgs), valid)
+        fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
+        d_loss = (real_loss + fake_loss) / 2
+
+        d_loss.backward()
+        optimizer_D.step()
+
+        print("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, opt.n_epochs, i, len(dataloader),
+                                                            d_loss.item(), g_loss.item()))
+        # for logging purposes
+        avg_d_real_loss += float(real_loss)
+        avg_d_fake_loss += float(fake_loss)
+        avg_d_total_loss += float(d_loss)
+        avg_g_loss += float(g_loss)
+        epoch_batches += 1
+
+        batches_done = epoch * len(dataloader) + i
+        if batches_done % opt.sample_interval == 0:
+            sample_images = gen_imgs.data[:25]
+            save_image(sample_images, 'images/%d.png' % batches_done, nrow=5, normalize=True)
+            if opt.logging:
+                sample_grid = make_grid(sample_images, nrow=5, normalize=True, scale_each=False, padding=2, pad_value=0)
+                viz_image_logger.image(sample_grid, opts=dict(title='%s_b_%d' % (opt.dataset, batches_done)))
+
+    # log epoch losses
+    avg_d_real_loss /= epoch_batches
+    avg_d_fake_loss /= epoch_batches
+    avg_d_total_loss /= epoch_batches
+    avg_g_loss /= epoch_batches
+    if opt.logging:
+        d_real_loss_logger.log(epoch, avg_d_real_loss)
+        d_fake_loss_logger.log(epoch, avg_d_fake_loss)
+        d_total_loss_logger.log(epoch, avg_d_total_loss)
+        g_loss_logger.log(epoch, avg_g_loss)
+
+    # checkpoint
+    torch.save(generator.state_dict(), "g_model")
+    torch.save(discriminator.state_dict(), "d_model")
--- dataset_test.py
+++ dataset_test.py
@@ -11,8 +11,8 @@ parser.add_argument('--dataset', type=str, default='cifar10', help='dataset name
 opt = parser.parse_args()
 
 dataloader = helper.load_dataset(opt.dataset, opt.img_size, opt.batch_size)
-viz = Visdom(port=8080, env="main")
-viz2 = Visdom(port=8080, env="images")
+# viz = Visdom(port=8080, env="main")
+# viz2 = Visdom(port=8080, env="images")
 for i, (imgs, _) in enumerate(dataloader):
     if i == 1:
         break
@@ -20,7 +20,7 @@ for i, (imgs, _) in enumerate(dataloader):
                      normalize=True, scale_each=False)
     grid2 = make_grid(imgs.data[25:50], nrow=5, padding=2, pad_value=0,
                      normalize=True, scale_each=False)
-    viz.image(grid, opts=dict(title='Random!', caption='How random.'))
-    viz2.image(grid2, opts=dict(title='Random2!', caption='How random2.'))
+    # viz.image(grid, opts=dict(title='Random!', caption='How random.'))
+    # viz2.image(grid2, opts=dict(title='Random2!', caption='How random2.'))
 
     save_image(imgs.data[:25], 'images/dataset_test.png', nrow=5, normalize=True)
