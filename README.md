122cdd8573c50941cd930cab027a38f48dc1892e
edolev89 <edolev89@gmail.com>
2018-12-02 21:48:05 -0500

    custom

---
+++ README.md
@@ -0,0 +1,2 @@
+python3 train_dcgan.py --dataset=celeba --n_epochs=100 --img_size=64 --channels=3 --logging=True --batch_size=128
+
--- helper.py
+++ helper.py
@@ -63,7 +63,17 @@ def load_dataset(dataset_name, image_size, batch_size):
         dataloader = torch.utils.data.DataLoader(
             datasets.ImageFolder(path,
                                  transform=transforms.Compose([
-                                     transforms.CenterCrop(96),
+                                     transforms.CenterCrop(128),
+                                     transforms.Resize(image_size),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize(normal_mean, normal_std)
+                                 ])),
+            batch_size=batch_size, shuffle=True)
+    elif dataset_name == 'custom':
+        dataloader = torch.utils.data.DataLoader(
+            datasets.ImageFolder(path,
+                                 transform=transforms.Compose([
+                                     transforms.CenterCrop(128),
                                      transforms.Resize(image_size),
                                      transforms.ToTensor(),
                                      transforms.Normalize(normal_mean, normal_std)
